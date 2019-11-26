4182ae3d9018d42f23bc2a36c413beed12a0d48c
sousaj8 <sousaj8@WITPF0TXFCR.wit.private>
2019-11-26 14:21:02 -0500

    Added eye detection

--- segmentation.m
+++ segmentation.m
@@ -1,7 +1,33 @@
-function [outputArg1,outputArg2] = segmentation(inputArg1,inputArg2)
-%SEGMENTATION Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+function [outputArg1] = segmentation(inputArg1)
+    img = inputArg1;
+    
+    % Viola Jones Detectors using Computer Vision Toolbox
+    faceDetector = vision.CascadeObjectDetector();
+    eyeDetector = vision.CascadeObjectDetector('EyePairSmall');
+    bb = step(faceDetector,img);
+    
+    %Detect the biggest box, which is the face
+    bigBox=1;     
+         for i=1:rank(bb) 
+             if bb(i,3)>bb(bigBox,3)
+                 bigBox=i;
+             end
+         end
+    
+    faceImage = imcrop(img,bb(bigBox,:));
+    bbEyes = step(eyeDetector,faceImage);
+    
+    %Find biggest Eyes;
+    bigEyes = 1;
+    for i = 1:rank(bbEyes)
+        if bbEyes(i,3) > bbEyes(bigEyes,3)
+            bigEyes = i;
+        end
+    end
+    
+    bbOneEye=[bbEyes(bigEyes,1),bbEyes(bigEyes,2),bbEyes(bigEyes,3)/3,bbEyes(bigEyes,4)];   %resize the eyepair width in half
+    eye = imcrop(faceImage,bbOneEye(1,:));
+    outputArg1 = eye;
+    
 end
 
